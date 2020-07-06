9a8720d36b35d219fdb953335ec722fbafe83190
Matt Barringer <matt@incoherent.de>
2020-07-06 09:44:27 +0200

    Show markers for car pods again

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/core/RxPicasso.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/core/RxPicasso.kt
@@ -1,40 +1,35 @@
 package com.skedgo.tripkit.ui.core
 
 import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import androidx.annotation.DimenRes
 import com.squareup.picasso.Picasso
-import com.squareup.picasso.Target
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
 import io.reactivex.schedulers.Schedulers
 
+
 fun Picasso.fetchAsync(url: String): Single<Bitmap> =
-    fetchAsyncWithSize(url, null)
+        fetchAsyncWithSize(url, null).observeOn(Schedulers.io()).subscribeOn(mainThread())
 
 fun Picasso.fetchAsyncWithSize(url: String, @DimenRes maxSize: Int? = null): Single<Bitmap> {
-  return Single
-      .create<Bitmap> { emitter ->
-        val target: Target = object : Target {
-          override fun onPrepareLoad(placeHolderDrawable: Drawable?) {}
-
-          override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) =
-              emitter.onError(UnableToFetchBitmapError("Unable to fetch bitmap $url"))
-
-          override fun onBitmapLoaded(bitmap: Bitmap, from: Picasso.LoadedFrom) =
-              emitter.onSuccess(bitmap)
+    return Single.create {
+        try {
+            if (!it.isDisposed) {
+                val creator = this.load(url)
+                maxSize?.let {
+                    creator.resizeDimen(it,it)
+                            .centerInside()
+                            .onlyScaleDown()
+                }
+                val bitmap = creator.get()
+                it.onSuccess(bitmap)
+            }
+        } catch (e: Throwable) {
+            it.onError(UnableToFetchBitmapError("Unable to fetch bitmap $url: ${e.message}"))
         }
-        val creator = this.load(url)
-        maxSize?.let {
-          creator.resizeDimen(it, it)
-              .centerInside()
-              .onlyScaleDown()
-        }
-        creator.into(target)
-        emitter.setCancellable { this.cancelRequest(target) }
-      }
-      .subscribeOn(mainThread())
-      .observeOn(Schedulers.io())
+    }
 }
+
+
 
 class UnableToFetchBitmapError(message: String) : RuntimeException(message)
--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/map/CreateMarkerForCarPod.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/map/CreateMarkerForCarPod.kt
@@ -14,6 +14,7 @@ import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import com.skedgo.tripkit.locations.CarPod
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 object CreateMarkerForCarPod {
 
@@ -27,6 +28,7 @@ object CreateMarkerForCarPod {
               .map { BitmapDescriptorFactory.fromBitmap(it) }
               .toObservable()
               .observeOn(Schedulers.io())
+                  .subscribeOn(AndroidSchedulers.mainThread())
               .onErrorResumeNext(Observable.empty())
         }
         .switchIfEmpty(Observable.fromCallable {
