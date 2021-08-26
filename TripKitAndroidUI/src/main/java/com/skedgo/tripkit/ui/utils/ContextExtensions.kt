6471120c36fc43fb3fe2ccfe2a804dc05cb75633
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2021-08-26 20:44:53 +0800

    :sparkles: Add Camera and Gallery selection for updating profile picture

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/utils/ContextExtensions.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/utils/ContextExtensions.kt
@@ -1,16 +1,32 @@
 package com.skedgo.tripkit.ui.utils
+
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.Uri
 import android.provider.Settings
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.bottomsheets.BasicGridItem
+import com.afollestad.materialdialogs.bottomsheets.BottomSheet
+import com.afollestad.materialdialogs.bottomsheets.GridItem
+import com.afollestad.materialdialogs.bottomsheets.gridItems
+import com.afollestad.materialdialogs.list.listItems
 
 fun Context.viewAppDetailsSettingsIntent(): Intent = Intent(
-    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-    Uri.fromParts("package", this.packageName, null)
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", this.packageName, null)
 ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
 fun Context.isNetworkConnected(): Boolean {
-  val cm: ConnectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-  return cm.activeNetworkInfo?.isConnectedOrConnecting ?: false
+    val cm: ConnectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    return cm.activeNetworkInfo?.isConnectedOrConnecting ?: false
+}
+
+fun Context.showSingleSelectionPopUpDialog(items: List<String>, onItemSelected: (String) -> Unit) {
+    MaterialDialog(this).show {
+        listItems(items = items, selection = { dialog, _, text ->
+            onItemSelected.invoke(text.toString())
+            dialog.dismiss()
+        })
+    }
 }
