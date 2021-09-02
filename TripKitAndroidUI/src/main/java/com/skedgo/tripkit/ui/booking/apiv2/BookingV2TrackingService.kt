e7b9775ae6b942ac54c1b19a4d0d756a32a063b1
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2021-09-02 21:49:58 +0800

    :bug: add google maps on manifest queries

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/booking/apiv2/BookingV2TrackingService.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/booking/apiv2/BookingV2TrackingService.kt
@@ -23,8 +23,4 @@ class BookingV2TrackingService(private var trackingApi: BookingV2TrackingApi) {
     suspend fun getActiveBooking(mode: String? = null): NetworkResponse<BookingV2ListResponse.Booking, Unit> {
         return trackingApi.getActiveBooking(mode)
     }
-
-    suspend fun getActiveBooking(mode: String): NetworkResponse<BookingV2ListResponse.Booking, Unit> {
-        return trackingApi.getActiveBooking(mode)
-    }
 }
