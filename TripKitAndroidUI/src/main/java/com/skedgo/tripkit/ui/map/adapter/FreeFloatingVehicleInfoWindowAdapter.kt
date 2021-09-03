c727fbddee6c956233f62d5af681143947d4a0c6
Leng Ann Goh <lengann@fbmsoftware.com>
2021-09-03 22:49:59 +0800

    Refactor LocationResponse to implement the new vehicle type response parameter and update the database

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/map/adapter/FreeFloatingVehicleInfoWindowAdapter.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/map/adapter/FreeFloatingVehicleInfoWindowAdapter.kt
@@ -1,4 +1,5 @@
 package com.skedgo.tripkit.ui.map.adapter
+
 import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
@@ -14,40 +15,50 @@ import com.skedgo.tripkit.ui.map.FreeFloatingVehiclePOILocation
 import com.skedgo.tripkit.ui.trippreview.nearby.title
 
 class FreeFloatingVehicleInfoWindowAdapter(private val context: Context) : StopInfoWindowAdapter {
-  val view: View by lazy { LayoutInflater.from(context).inflate(R.layout.free_floating_vehicle_info_window, null, false) }
+    val view: View by lazy { LayoutInflater.from(context).inflate(R.layout.free_floating_vehicle_info_window, null, false) }
 
-  @SuppressLint("InflateParams")
-  override fun getInfoWindow(marker: Marker): View {
-    val entity = (marker.tag as FreeFloatingVehiclePOILocation).freeFloatingLocationEntity.vehicle
-    view.findViewById<TextView>(R.id.operatorName).text = entity.operator.name
-    val vehicleType = entity.sharedVehicleType()
-    val vehicleTypeView = view.findViewById<TextView>(R.id.type)
-    vehicleTypeView.text = context.getString(vehicleType.title())
-    vehicleTypeView.setCompoundDrawablesWithIntrinsicBounds(vehicleType.iconId, 0,0,0)
-    view.findViewById<TextView>(R.id.name).text = entity.name
-    view.findViewById<TextView>(R.id.batteryLevel).text = String.format("%d%%", entity.batteryLevel)
-    view.findViewById<TextView>(R.id.batteryLevelText).setCompoundDrawablesWithIntrinsicBounds(
-            when {
-              entity.batteryLevel < 12 -> R.drawable.ic_battery_0
-              entity.batteryLevel < 37 -> R.drawable.ic_battery_25
-              entity.batteryLevel < 62 -> R.drawable.ic_battery_50
-              entity.batteryLevel < 87 -> R.drawable.ic_battery_75
-              else -> R.drawable.ic_battery_100
-            }, 0,0,0)
-    return view
-  }
+    @SuppressLint("InflateParams")
+    override fun getInfoWindow(marker: Marker): View {
+        val entity = (marker.tag as FreeFloatingVehiclePOILocation).freeFloatingLocationEntity.vehicle
+        view.findViewById<TextView>(R.id.operatorName).text = entity.operator.name
+        val vehicleType = entity.sharedVehicleType()
+        val vehicleTypeView = view.findViewById<TextView>(R.id.type)
+        vehicleTypeView.text = context.getString(vehicleType.title())
+        vehicleTypeView.setCompoundDrawablesWithIntrinsicBounds(vehicleType.iconId, 0, 0, 0)
+        view.findViewById<TextView>(R.id.name).text = entity.name
+        view.findViewById<TextView>(R.id.batteryLevel).text = String.format("%d%%", entity.batteryLevel)
+        view.findViewById<TextView>(R.id.batteryLevelText).setCompoundDrawablesWithIntrinsicBounds(
+                when {
+                    entity.batteryLevel < 12 -> R.drawable.ic_battery_0
+                    entity.batteryLevel < 37 -> R.drawable.ic_battery_25
+                    entity.batteryLevel < 62 -> R.drawable.ic_battery_50
+                    entity.batteryLevel < 87 -> R.drawable.ic_battery_75
+                    else -> R.drawable.ic_battery_100
+                }, 0, 0, 0)
+        return view
+    }
 
-  override fun getInfoContents(marker: Marker): View? {
-    return null
-  }
+    override fun getInfoContents(marker: Marker): View? {
+        return null
+    }
 
-  override fun onInfoWindowClosed(marker: Marker) {}
+    override fun onInfoWindowClosed(marker: Marker) {}
 
-  override fun windowInfoHeightInPixel(marker: Marker): Int = view.height
+    override fun windowInfoHeightInPixel(marker: Marker): Int = view.height
 }
 
 fun FreeFloatingVehicleEntity.sharedVehicleType(): SharedVehicleType {
-  return SharedVehicleType.valueOf(this.vehicleType)
+    var vehicleType = this.vehicleType
+    this.vehicleTypeInfo?.let {
+        vehicleType = it.formFactor
+    }
+
+    val sharedVehicleType = SharedVehicleType.valueOf(vehicleType)
+    return if (sharedVehicleType == SharedVehicleType.SCOOTER) {
+        SharedVehicleType.KICK_SCOOTER
+    } else {
+        sharedVehicleType
+    }
 }
 
 
--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/trippreview/nearby/SharedVehicleTitle.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/trippreview/nearby/SharedVehicleTitle.kt
@@ -1,8 +1,6 @@
 package com.skedgo.tripkit.ui.trippreview.nearby
 
-import android.content.Context
 import com.skedgo.tripkit.common.model.SharedVehicleType
-import com.skedgo.tripkit.routing.VehicleMode
 import com.skedgo.tripkit.ui.R
 
 
@@ -10,7 +8,7 @@ fun SharedVehicleType.title(): Int {
     return when (this) {
         SharedVehicleType.BIKE -> R.string.bicycle
         SharedVehicleType.CAR -> R.string.car
-        SharedVehicleType.KICK_SCOOTER-> R.string.kick_scooter
+        SharedVehicleType.KICK_SCOOTER, SharedVehicleType.SCOOTER-> R.string.kick_scooter
         SharedVehicleType.MOTO_SCOOTER -> R.string.moto_scooter
         SharedVehicleType.PEDELEC -> R.string.e_minusbike
     }
