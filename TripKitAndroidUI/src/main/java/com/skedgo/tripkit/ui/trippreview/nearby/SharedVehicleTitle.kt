bb59b5538dd6b7e2735d4f0e8d81621dd82e7ec4
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2021-09-06 09:34:17 +0800

    :bug: add handling for Vehicle Info BICYCLE type

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/trippreview/nearby/SharedVehicleTitle.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/trippreview/nearby/SharedVehicleTitle.kt
@@ -7,6 +7,7 @@ import com.skedgo.tripkit.ui.R
 fun SharedVehicleType.title(): Int {
     return when (this) {
         SharedVehicleType.BIKE -> R.string.bicycle
+        SharedVehicleType.BICYCLE -> R.string.bicycle
         SharedVehicleType.CAR -> R.string.car
         SharedVehicleType.KICK_SCOOTER, SharedVehicleType.SCOOTER-> R.string.kick_scooter
         SharedVehicleType.MOTO_SCOOTER -> R.string.moto_scooter
