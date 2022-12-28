d8031b17959cdfec8fcee163c74adf3741b535a6
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2022-12-28 13:48:32 +0800

    region route autocompleter update

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/routing/autocompleter/sample/RouteAutoCompleteSampleFragment.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/routing/autocompleter/sample/RouteAutoCompleteSampleFragment.kt
@@ -3,6 +3,7 @@ package com.skedgo.tripkit.ui.routing.autocompleter.sample
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -30,7 +31,7 @@ class RouteAutoCompleteSampleFragment : BaseFragment<FragmentRouteAutoCompletSam
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         viewModel.regionRoutes.observe(this) {
-            
+            Log.e("RouteAutoCompleteSample", gson.toJson(it))
         }
     }
 
