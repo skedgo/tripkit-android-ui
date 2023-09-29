4cb6a00ed2cb5423c03a63083e575fe525568278
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2023-09-30 02:55:02 +0800

    [19696] RoadTag labels on direction

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/tripresult/RoadTagChartItem.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/tripresult/RoadTagChartItem.kt
@@ -1,9 +1,11 @@
 package com.skedgo.tripkit.ui.tripresult
 
+import android.graphics.Color
 import com.skedgo.tripkit.routing.RoadTag
 
 data class RoadTagChartItem(
     val label: String,
     val length: Int,
-    val color: Int
+    val color: Int,
+    val textColor: Int = Color.WHITE
 )
