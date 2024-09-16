343e55040d344a66d22789462211ea9dc9bfa924
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2024-09-17 00:15:15 +0800

    [22270] - [TripGov5] update BalanceItemViewModel and DrtViewModel to use currency symbol for showing price - [TripKitUI] update Common.kt to include aud currency symbol

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/utils/Common.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/utils/Common.kt
@@ -3,6 +3,9 @@ package com.skedgo.tripkit.ui.utils
 //To handle currency symbol if passed is words.
 //Add more currency to handle
 fun String.getCurrencySymbol(): String {
-    return if (this == "USD") "$"
-    else this
+    return when {
+        this == "USD" -> "$"
+        this == "AUD" -> "AU$"
+        else -> this
+    }
 }
