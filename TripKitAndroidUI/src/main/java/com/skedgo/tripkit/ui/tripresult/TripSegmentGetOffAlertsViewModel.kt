5993610fc3cb637aba6ca50a2a5fd8e3a9f45773
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2024-10-31 02:19:22 +0800

    [22841] - [TripKitUI] update trip_result_list_item.xml to set scrollbars to none [22847] - [TripKitUI] update TripSegmentGetOffAlertDetailViewModel to add dialog to give instructions on how they can allow background location access

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/tripresult/TripSegmentGetOffAlertsViewModel.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/tripresult/TripSegmentGetOffAlertsViewModel.kt
@@ -6,7 +6,9 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Build
+import android.provider.Settings
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.DiffUtil
@@ -292,8 +294,24 @@ class TripSegmentGetOffAlertsViewModel @Inject internal constructor(
             }
 
             if (it.denied.isNotEmpty()) {
+
                 _getOffAlertStateOn.postValue(false)
                 pendingIntent?.let { intent -> alarmManager.cancel(intent) }
+
+                context.showConfirmationPopUpDialog(
+                    title = context.getString(R.string.confirmation_allow_background_location_title),
+                    message = context.getString(R.string.confirmation_allow_background_location_message),
+                    positiveLabel = context.getString(R.string.continue_),
+                    positiveCallback = {
+                        val intent = Intent(
+                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.fromParts("package", context.packageName, null)
+                        )
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        context.startActivity(intent)
+                    },
+                    negativeLabel = context.getString(R.string.cancel)
+                )
             } else {
 
                 val reminder = TimeUnit.MINUTES.toSeconds(reminderInMinutes)
