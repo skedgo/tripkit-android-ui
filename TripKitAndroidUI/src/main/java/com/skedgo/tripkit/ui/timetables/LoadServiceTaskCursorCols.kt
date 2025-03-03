85dce02d7aeb9ee0069b75a693c2dec88688373d
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2025-03-04 02:16:54 +0800

    [23022] - [TripKitUI] convert LoadServiceTaskCursorCols.java to kotlin - [TripKitUI] add LoadServiceTaskTest

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/timetables/LoadServiceTaskCursorCols.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/timetables/LoadServiceTaskCursorCols.kt
@@ -1,44 +1,41 @@
-package com.skedgo.tripkit.ui.timetables;
+package com.skedgo.tripkit.ui.timetables
 
-import android.database.Cursor;
-
-import com.skedgo.tripkit.data.database.DbFields;
-
+import android.database.Cursor
+import com.skedgo.tripkit.data.database.DbFields
 
 /**
  * TODO Should find an appropriate class name
  */
-public class LoadServiceTaskCursorCols {
+object LoadServiceTaskCursorCols {
+    var id: Int = -1
+    var waypoints: Int = -1
+    var lat: Int = -1
+    var lon: Int = -1
+    var bearing: Int = -1
+    var address: Int = -1
+    var name: Int = -1
+    var departureTime: Int = -1
+    var arrivalTime: Int = -1
+    var stopCode: Int = -1
+    var realTimeStatus: Int = -1
+    var travelled: Int = -1
+    var serviceColor: Int = -1
+    var wheelchairAccessible: Int = -1
 
-    public static int id = -1;
-    public static int waypoints = -1;
-    public static int lat = -1;
-    public static int lon = -1;
-    public static int bearing = -1;
-    public static int address = -1;
-    public static int name = -1;
-    public static int departureTime = -1;
-    public static int arrivalTime = -1;
-    public static int stopCode = -1;
-    public static int realTimeStatus = -1;
-    public static int travelled = -1;
-    public static int serviceColor = -1;
-    public static int wheelchairAccessible = -1;
-
-    public static void init(Cursor cursor) {
-        id = cursor.getColumnIndex(DbFields.ID.name);
-        waypoints = cursor.getColumnIndex(DbFields.WAYPOINT_ENCODING.name);
-        travelled = cursor.getColumnIndex(DbFields.TRAVELLED.name);
-        lat = cursor.getColumnIndex(DbFields.LAT.name);
-        lon = cursor.getColumnIndex(DbFields.LON.name);
-        bearing = cursor.getColumnIndex(DbFields.BEARING.name);
-        address = cursor.getColumnIndex(DbFields.ADDRESS.name);
-        name = cursor.getColumnIndex(DbFields.NAME.name);
-        departureTime = cursor.getColumnIndex(DbFields.DEPARTURE_TIME.name);
-        arrivalTime = cursor.getColumnIndex(DbFields.ARRIVAL_TIME.name);
-        stopCode = cursor.getColumnIndex(DbFields.STOP_CODE.name);
-        realTimeStatus = cursor.getColumnIndex(DbFields.REAL_TIME_STATUS.name);
-        serviceColor = cursor.getColumnIndex(DbFields.SERVICE_COLOR.name);
-        wheelchairAccessible = cursor.getColumnIndex(DbFields.WHEELCHAIR_ACCESSIBLE.name);
+    fun init(cursor: Cursor) {
+        id = cursor.getColumnIndex(DbFields.ID.name)
+        waypoints = cursor.getColumnIndex(DbFields.WAYPOINT_ENCODING.name)
+        travelled = cursor.getColumnIndex(DbFields.TRAVELLED.name)
+        lat = cursor.getColumnIndex(DbFields.LAT.name)
+        lon = cursor.getColumnIndex(DbFields.LON.name)
+        bearing = cursor.getColumnIndex(DbFields.BEARING.name)
+        address = cursor.getColumnIndex(DbFields.ADDRESS.name)
+        name = cursor.getColumnIndex(DbFields.NAME.name)
+        departureTime = cursor.getColumnIndex(DbFields.DEPARTURE_TIME.name)
+        arrivalTime = cursor.getColumnIndex(DbFields.ARRIVAL_TIME.name)
+        stopCode = cursor.getColumnIndex(DbFields.STOP_CODE.name)
+        realTimeStatus = cursor.getColumnIndex(DbFields.REAL_TIME_STATUS.name)
+        serviceColor = cursor.getColumnIndex(DbFields.SERVICE_COLOR.name)
+        wheelchairAccessible = cursor.getColumnIndex(DbFields.WHEELCHAIR_ACCESSIBLE.name)
     }
 }
