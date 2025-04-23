6aa72d24c0a4f2d00ed78e8194294ccd1920ad09
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2025-04-23 17:46:18 +0800

    [23648] - [TripGo] Bump up Catch a Ride version

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/menu/data/TKUIMenuData.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/menu/data/TKUIMenuData.kt
@@ -1,13 +1,15 @@
 package com.skedgo.tripkit.ui.menu.data
 
 abstract class TKUIMenuData : TKUIMenuItem {
-//    data class ActiveBooking(val vm: ActiveBookingItemViewModel) : TKUIMenuData() {
-//        override fun getViewType() = ACTIVE_BOOKING_VIEW_TYPE
-//    }
-//
-//    data class TransitTicket(val vm: TransitTicketItemViewModel) : TKUIMenuData() {
-//        override fun getViewType() = TRANSIT_TICKET_VIEW_TYPE
-//    }
+    /* Commented out for now. Still finding a better implementation.
+    data class ActiveBooking(val vm: ActiveBookingItemViewModel) : TKUIMenuData() {
+        override fun getViewType() = ACTIVE_BOOKING_VIEW_TYPE
+    }
+
+    data class TransitTicket(val vm: TransitTicketItemViewModel) : TKUIMenuData() {
+        override fun getViewType() = TRANSIT_TICKET_VIEW_TYPE
+    }
+    */
 
     companion object {
         const val ACTIVE_BOOKING_VIEW_TYPE = 1
