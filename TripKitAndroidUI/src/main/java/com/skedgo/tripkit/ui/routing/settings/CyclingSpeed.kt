0b60aebc54df749714cfc9bcbd9490bb65967149
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2025-09-15 18:09:25 +0800

    [24473] - cycling speed handling and caching fixes

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/routing/settings/CyclingSpeed.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/routing/settings/CyclingSpeed.kt
@@ -1,12 +1,12 @@
 package com.skedgo.tripkit.ui.routing.settings
 
 enum class CyclingSpeed(val value: Int) {
-    Slow(0), Medium(1), Fast(2)
+    Slow(0), Medium(1), Fast(2), Impaired(-1)
 }
 
 fun Int.toCyclingSpeed(): CyclingSpeed = when (this) {
     0 -> CyclingSpeed.Slow
     1 -> CyclingSpeed.Medium
     2 -> CyclingSpeed.Fast
-    else -> throw IllegalArgumentException("Unable to convert to CyclingSpeed for $this.")
+    else -> CyclingSpeed.Impaired
 }
