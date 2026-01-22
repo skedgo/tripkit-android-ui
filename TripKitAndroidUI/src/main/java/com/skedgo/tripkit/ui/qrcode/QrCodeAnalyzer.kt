e4cdd874ae4af58bad9231b89d014636d225435f
sg.jsonjuliane <jason@skedgo.com>
2026-01-23 03:51:37 +0800

    [24958] Fix code changes for 16KB compliance dependencies
    - Update ML Kit Barcode import to common package (17.3.0 API change)
    - Fix type inference in QrCodeScanActivity
    - Improve null safety in ProviderUtils

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/qrcode/QrCodeAnalyzer.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/qrcode/QrCodeAnalyzer.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
-import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
@@ -12,7 +12,7 @@ import com.google.mlkit.vision.common.InputImage
 
 class QrCodeAnalyzer(private val onQrCodesDetected: (qrCodes: List<Barcode>) -> Unit) :
     ImageAnalysis.Analyzer {
-    @SuppressLint("UnsafeExperimentalUsageError")
+    @SuppressLint("UnsafeExperimentalUsageError", "UnsafeOptInUsageError")
     override fun analyze(imageProxy: ImageProxy) {
         val mediaImage = imageProxy.image
         if (mediaImage != null) {
