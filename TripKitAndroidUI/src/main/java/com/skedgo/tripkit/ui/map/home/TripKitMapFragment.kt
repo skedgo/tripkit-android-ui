f70eb700254f35542c9a5a86e3e7daa9a1009f8b
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2026-03-16 22:47:15 +0800

    [25327] - favorite re-draw fix

--- TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/map/home/TripKitMapFragment.kt
+++ TripKitAndroidUI/src/main/java/com/skedgo/tripkit/ui/map/home/TripKitMapFragment.kt
@@ -435,17 +435,6 @@ class TripKitMapFragment : LocationEnhancedMapFragment(), OnInfoWindowClickListe
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ (first, second) ->
-                if (second.isNotEmpty()) {
-                    poiMarkers?.markers
-                        ?.filter { marker ->
-                            val poi = marker.tag as? IMapPoiLocation ?: return@filter false
-                            poi.identifier in second
-                        }
-                        ?.forEach { marker ->
-                            removeMarkerPosition(marker.position)
-                            marker.remove()
-                        }
-                }
                 for ((first1, second1) in first) {
                     // Check if a marker with the same position already exists
                     if (!isMarkerPositionExists(first1.position)) {
